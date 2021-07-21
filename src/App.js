f5e32a8293bcbab9741177b1c0d6a79520f7edae
Antonio1965 <saburo_tsutsumi@yahoo.com.ar>
2021-07-20 23:52:21 -0300

    Se agrega los archivos del proyectos.

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./Sass/app.css";
+
+import { useEffect } from "react";
+import { NavBar } from "./components/General/Navbar";
+import { Footer } from "./components/General/Footer";
+// import { ItemList } from './components/ItemList/ItemList';
+// import Item from "./components/ItemList/Item";
+import { ItemListContainer } from "./components/ItemList/ItemListContainer";
+// import { Loader } from "./Loader/Loader";
+import { Error404 } from "./components/General/Error404";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ItemDetailContainer } from "./components/ItemDetail/ItemDetailContainer";
+import { Counter } from './components/Counter/Counter';
+import { Checkout } from "./components/CheckoutForm/Checkout";
+
+// import { Counter } from "./componentes/Counter/Counter";
+
+export const App = () => {
+  
+  return (
+    <>
+      <Router>
+        <NavBar title="Drone.Co" />
+        <Switch>
+          <Route exact path="/">
+            <ItemListContainer />
+          </Route>
+          <Route path="/item/:id">
+            <ItemDetailContainer />
+          </Route>
+          <Route path='/checkout'>
+            <Checkout/>
+          </Route>
+          <Route  component = {Error404}/>
+          <Route path='/counter'>
+            <Counter/>
+          </Route>
+        </Switch>
+        <Footer autor="Copyright &copy; -Tsutsumi Jose Antonio - Junio 2021." />
+      </Router>
+    </>
+  );
+};
